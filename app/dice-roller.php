60a234e463b7a311079e940d6bc68f0ebf910ad5
worldofelisa <worldofelisa@gmail.com>
2022-01-24 14:52:02 +0000

    I have fixed the error where the code would not output. Code has been altered so it now outputs into a readable fashion.

--- app/dice-roller.php
+++ app/dice-roller.php
@@ -2,26 +2,57 @@
 
 class Dice {
     /**
+     * @param $numberOfFaces
      * @return int
      */
-    function diceRoller($numberOfFaces): int
+    function rollResult($numberOfFaces): int
     {
         return rand(1,$numberOfFaces);
     }
 
-    function readCommand($arguments)
+    /**
+     * @param $arguments
+     * @return array
+     */
+    function diceInterpreter($arguments): array
     {
+        $results = array();
         for ($x = 1; $x < count($arguments); $x++)
         {
-            $exploded = explode("d",$arguments[$x]);
-            for ($y = 1; $y <= $exploded[0]; $y++)
+            $diceParser = explode("d",$arguments[$x]);
+            for ($y = 1; $y <= $diceParser[0]; $y++)
             {
-                $this->diceRoller($exploded[1]);
+               $results[$diceParser[1]][] = $this->rollResult($diceParser[1]);
+                //Parser = breaks down data into smaller elements for easy translation
+                // x= the amount of arguments inputted into the command line
+                // y= counting the number of times the for loop is running
+                // diceParser[0] is counting the number of dice
+                // diceParser [1] is counting the number of faces and sending it through the dice roller method
             }
-
        }
+        return $results;
     }
 }
+
+class DiceDisplay {
+    /**
+     * @param array $rollResults
+     * @return void
+     */
+    public function render(array $rollResults)
+    {
+        foreach ($rollResults as $faces => $results)
+        {
+
+            foreach ($results as $result)
+            {
+                echo "I rolled a d$faces and it was a $result\n";
+            }
+        }
+    }
+}
+
 $rollADie = new Dice();
-$rollADie->readCommand($argv);
-//incomplete because it isn't outputting. Will be fixed in an upload later today.
+$rollResults = $rollADie->diceInterpreter($argv);
+$display = new DiceDisplay();
+$display->render($rollResults);
